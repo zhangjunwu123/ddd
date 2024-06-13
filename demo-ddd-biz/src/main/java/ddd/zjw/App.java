1a42dc67b9d0985dd7c47a53c1310940c779f592
zhangjunwu123 <865006255@qq.com>
2024-06-13 22:20:41 +0800

    修改与查询

---
+++ demo-ddd-biz/src/main/java/ddd/zjw/App.java
@@ -0,0 +1,13 @@
+package ddd.zjw;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
